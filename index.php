2673dd349c5daf59b2757fd1b21f427203c12eb1
Derek Deike <derek.deike@gmail.com>
2013-09-15 23:25:25 -0700

    Text edits

--- index.php
+++ index.php
@@ -10,10 +10,9 @@
 	</head>
 	<body>
 		<p>Derek Deike</p>
-		<p>I
-		am raising three boys in San Francisco with my lovely wife, everything else is
+		<p>I am raising three boys in San Francisco with my lovely wife, everything else is
 		a pleasant blur. Background in html and css. There is going to be a lot of new
-		material for me in this course.</p>
+		material for me in this course. </p>
 		<p>Created on Sublime Text 2</p>
 	</body>
 </html>
