11eb0fdf7f44dc5529d49f91aabe98311d0485e0
Derek Deike <derek.deike@gmail.com>
2013-09-15 23:03:43 -0700

    Added index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML> <html>
+	<head>
+		<meta http-equiv="Content-Type"
+		content="text/html; charset=utf-8">
+		<style type="text/css"
+		media="screen">
+		</style>
+		<title>P1 Assignment Derek
+		Deike</title>
+	</head>
+	<body>
+		<p>Derek Deike</p>
+		<p>I
+		am raising three boys in San Francisco with my lovely wife, everything else is
+		a pleasant blur. Background in html and css. There is going to be a lot of new
+		material for me in this course.</p>
+		<p>Created on Sublime Text 2</p>
+	</body>
+</html>
